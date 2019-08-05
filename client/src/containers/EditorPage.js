c90d7ae546473bcf90f0ca7e1d9c4742c1653d6f
Georgiy Pyantkovsky <georgiy.pyantkovsky.ukr@gmail.com>
2019-08-05 17:00:17 -0400

    minor fixes

--- client/src/containers/EditorPage.js
+++ client/src/containers/EditorPage.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Editor, EditorState } from "draft-js";
 
 import EditBox from "../components/EditBox";
-import Headers from "../components/headers";
+import Headers from "../components/Headers";
 
 function EditorPage() {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
