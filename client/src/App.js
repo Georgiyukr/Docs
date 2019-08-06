aa9be31e79534f75fe8bf64859652a9b65660735
git push -u origin <barbara.ogyasi@gmail.com>
2019-08-06 16:57:45 -0400

    prepping to test portal page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import EditorPage from "./containers/EditorPage";
-import Login from "./containers/Login"
+import Login from "./containers/Login";
 
 function App() {
   return (
     <div>
-      <EditorPage />
-      
+      {/* <EditorPage /> */}
+
+      <Login />
     </div>
   );
 }
