10f97f3469935acead18fa1454342e006b87b80c
git push -u origin <barbara.ogyasi@gmail.com>
2019-08-06 15:09:04 -0400

    made user and doc models

--- client/src/components/EditBox.js
+++ client/src/components/EditBox.js
@@ -4,20 +4,6 @@ import { Editor, EditorState } from "draft-js";
 import Toolbar from "./Toolbar";
 
 function EditBox({ editorState, onChange }) {
-  // const editBoxStyle = {
-  //   background: "#fff",
-  //   border: "3px solid #ddd",
-  //   fontFamily: "Georgia, serif",
-  //   fontSize: "14px",
-  //   padding: "15px"
-  // };
-  // const inputBoxStyle = {
-  //   borderTop: "1px solid #ddd",
-  //   cursor: "text",
-  //   fontSize: "16px",
-  //   marginTop: "10px"
-  // };
-
   const editBoxStyle = {
     background: "#fff",
     border: "3px solid #ddd",
